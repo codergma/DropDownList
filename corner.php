6f6a82a17adc69795dbd7dbcaa93fb7de04b77e1
liubin <liubin@mapgis.com>
2015-09-05 13:14:29 +0800

    add corner.php

---
+++ corner.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<style>
+.corner{
+border-left: 10px solid red;
+border-right: 10px solid yellow;
+border-top: 10px solid blue;
+border-bottom: 10px solid green;
+width:0px;
+}
+.corner2{
+border-left: 10px solid transparent;
+border-right: 10px solid transparent;
+border-top: 10px solid transparent;
+border-bottom: 10px solid green;
+width:0px;
+}
+.corner3{
+border-left: 10px solid transparent;
+border-right: 10px solid transparent;
+border-bottom: 10px solid green;
+width:0px;
+}
+
+</style>
+</head>
+<body>
+<div class="corner">
+</div>
+<div class="corner2">
+</div>
+<div class="corner3">
+</div>
+</body>
+</html>
