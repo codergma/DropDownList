132bd5c09f608fd3ff79e18e26a4de33f84a58d6
liubin <liubin@mapgis.com>
2015-09-06 17:31:05 +0800

    尖角

--- corner.php
+++ corner.php
@@ -23,15 +23,30 @@ border-right: 10px solid transparent;
 border-bottom: 10px solid green;
 width:0px;
 }
+.corner4{
+	width:0px;
+	height:0px;
+	border-top:6px solid #dedede;
+	border-right:10px solid #dedede;
+	border-left:10px solid #fff;
+	border-bottom:6px solid #fff;
+	background:#c3dced;
+}
 
 </style>
 </head>
 <body>
 <div class="corner">
 </div>
+<hr/>
 <div class="corner2">
 </div>
 <div class="corner3">
 </div>
+<hr/>
+<div class="corner4">
+</div>
+<hr/>
+
 </body>
 </html>
